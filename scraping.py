70d93d5deda85cb10ac6b8568f64a4d178599ca2
Joshua Swinney <joshua_swinney1@baylor.edu>
2022-04-20 03:47:16 -0500

    webscrpaing

--- scraping.py
+++ scraping.py
@@ -1,9 +1,10 @@
+from unicodedata import name
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 
 
 
-url = 'https://cryptowat.ch/assets'
+url = 'https://www.coingecko.com/'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
 
 req = Request(url, headers=headers)
@@ -14,25 +15,38 @@ soup = BeautifulSoup(webpage, 'html.parser')
 
 title = soup.title
 
-print(title.text)
+print()
 
-#crypto_table = soup.find('table')
+print('-' * 67)
 
-#rows = crypto_table.findAll('tr')
+print(' ' + title.text)
 
-'''
-for row in rows[:5]:
-    cols = row.findAll('td')
-    name = cols[2].text.strip()
-    ticker = cols[1]
-    c_price = float(cols[3].text)
-    p_change = float(cols[5].text.strip('%'))
+print('-' * 67)
 
-    s_price = round(c_price/(1+(p_change/100)),2)
+print()
 
-    print(f"Name: {name}")
-    print(f"Symbol: {ticker}")
-    print(f"Current Price: {c_price}")
-    print(f"24H Change: {p_change}")
-    print(f"Starting Price: {s_price}")
-'''
+crpyto_table = soup.findAll('tbody')
+
+crpyto_table = crpyto_table[0]
+
+table_rows = crpyto_table.findAll('tr')
+
+for row in table_rows[:5]:
+    cols = row.findAll("td")
+    name = cols[2].find("a",attrs={'class':"tw-hidden lg:tw-flex font-bold tw-items-center tw-justify-between"})
+    name = name.text.strip()
+    ticker = cols[2].find("span",attrs={'class':"tw-hidden d-lg-inline font-normal text-3xs ml-2"})
+    ticker = ticker.text.strip()
+    price = cols[4].text.strip('\n$')
+    price_float = float(price.replace(',',''))
+    twofour_delta1 = cols[6].text.strip()
+    twofour_delta2 = float(twofour_delta1.strip('%'))
+    twofour_price = round(price_float/(1+(twofour_delta2/100)),2)
+    #twofour_price = twofour_price('{:,.2f}'.format)
+
+    print(f"Name:      {name}")
+    print(f"Ticker:    {ticker}")
+    print(f"Price:     ${price}")
+    print(f"24H \u0394:     {twofour_delta1}")
+    print(f"24H Price: ${twofour_price}")
+    print()
